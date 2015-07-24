613c9e22fdf0e01cbe005b707c89a0d4760eccaa
Klecoz <arseniocolon@gmail.com>
2015-07-23 22:14:46 -0400

    Added a getData function that does all the JSON. Added function to the text box and button to get user query.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,47 @@
 $(document).ready(function(){
 
 //JSON request to Youtube
-  var url;
-  var ID;
+  var thumbnailUrl; //Holds image url for medium thumbnail
+  var ID; //Holds ID of video for linking
+  var videos; //Number of videos in search array.
+
+  var url = 'https://www.googleapis.com/youtube/v3/search';
   var params = {
     key: 'AIzaSyAFRBimlEH1PZiHUHVc40VOQMm9J8XqHD0',
     part: 'snippet',
     type: 'video',
-    q: 'eggs' //Search request.
+    q: ''
   };
-  url = 'https://www.googleapis.com/youtube/v3/search';
 
+function getData() {
   $.getJSON(url, params, function(data){
-    //Gets the url and ID from the search request.
-    url = data.items[0].snippet.thumbnails.medium.url;
-    ID = data.items[0].id.videoId;
-    $('#search-results').append("<a href='https://www.youtube.com/watch?v=" + ID + "'>" + "<img src='" + url + "'>" + "</a>");
+    console.log(data);
+    videos = data.items.length;
+
+    //Gets the url and ID from the search request and prints each in #search-results.
+    for (var i = 0; i <= videos; i++) {
+    thumbnailUrl = data.items[i].snippet.thumbnails.medium.url;
+    ID = data.items[i].id.videoId;
+    $('#search-results').append("<a href='https://www.youtube.com/watch?v=" + ID + "'>" + "<img src='" + thumbnailUrl + "'>" + "</a>");
+    }
   });
 
+}
 
 
 
+$('#button').on('click', function(event) {
+  event.preventDefault();
+  params.q = $('#query').val();
+  $('#search-results').empty();
+  getData();
+
+
+
+
+
+
+});
 
 
 
